f76e2d6e9f633f19c3aee1900061a05773333b67
Jordan Minatogawa <jordankm@jordankm-macbookpro.roam.corp.google.com>
2020-03-24 17:42:56 -0700

    need to sort the object lists now.

--- inf551_final_proj/app/controllers/queries_controller.rb
+++ inf551_final_proj/app/controllers/queries_controller.rb
@@ -29,14 +29,22 @@ class QueriesController < ApplicationController
 
 
   # response is a list of objects
+  # we return an object in the form { primary key value : { table_name : [ list of cols] }}
   def make_table_object_from_response(responses, token, table_name)
     object = Hash.new
     responses.each do |response|
-      puts "HI THERE"
-      puts response.class.name
-      #puts response["Primary Values"]
-      #primary_key_value = obj["Primary Values"].join("_")
+      primary_key_value = response["Primary Values"].join("_")
+      puts primary_key_value
+      if not object.has_key? primary_key_value
+        object[primary_key_value] = Hash.new
+      end
 
+      inner_object = object[primary_key_value]
+      if not inner_object.has_key? table_name
+        inner_object[table_name] = Array.new
+      end
+
+      inner_object[table_name].append(response["Column"])
     end
     return object
   end
@@ -44,9 +52,11 @@ class QueriesController < ApplicationController
 
 
   def get_list_for_table(table_name)
+    puts "table name = " + table_name
     tokens = get_tokens()
     objects = Array.new
     tokens.each do |token|
+      puts "token = " + token
       query_string = $firebase_url + "/" + @database_name + "/index/" + table_name + "/" + token + ".json?"
       response = HTTParty.get(query_string)
       response_obj = JSON.parse(response.body)
@@ -56,6 +66,7 @@ class QueriesController < ApplicationController
         puts "Empty response"
       end
     end
+    puts objects
     return objects
   end
   
@@ -64,6 +75,8 @@ class QueriesController < ApplicationController
     @table_names.each do |table_name|
       obj_list = obj_list.concat(get_list_for_table(table_name))
     end
+
+    ###### TODO NEED TO SORT THE LIST OF OBJECTS NOW
     return obj_list
   end
 
